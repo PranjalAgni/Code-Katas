d4a542c06ac9a8ee0b59b1453ae4afd536dad0db
PranjalAgni <pranjal.agnihotri6@gmail.com>
2021-09-29 23:40:02 +0530

    :sparkles: solved array-difference JS implementation based question

---
+++ 09-28-2021/paginating-huge-books.js
@@ -0,0 +1,28 @@
+function countDigits(num) {
+  return BigInt(num.toString().length);
+}
+
+function pageDigits(pages) {
+  let totalDigits = 0n;
+  let currentPage = 1n;
+  let digits = 1n;
+
+  while (currentPage < pages) {
+    let nextCurrentPage = BigInt(currentPage * 10n);
+    if (nextCurrentPage > pages) {
+      nextCurrentPage = pages;
+    }
+
+    const numbersInRange = nextCurrentPage - currentPage;
+    totalDigits += digits * numbersInRange;
+    digits += 1n;
+
+    currentPage = nextCurrentPage;
+  }
+
+  return totalDigits + countDigits(currentPage);
+}
+
+console.log(pageDigits(4n), 4n);
+console.log(pageDigits(12n), 15n);
+console.log(pageDigits(100n), 192n);
