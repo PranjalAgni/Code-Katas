f7c2e00ccffe725a890d187f0da69499d9e11fce
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-02-13 00:06:58 +0530

    :rocket: solved reverse process with modulus inverse approach

--- 2-12-2022/reverse-process.js
+++ 2-12-2022/reverse-process.js
@@ -6,46 +6,33 @@ function getCharacterFromPosition(position) {
   return String.fromCharCode(97 + position);
 }
 
-function solveEquation(num, position) {
-  // m --> 12,   12 * 6015 % 26 = 4,
-  for (let m = 0; m <= 25; m++) {
-    const value = (m * num) % 26;
-    if (value === position) {
-      console.log(`For m = ${m}, value = ${value}`);
-      return m;
+// https://rosettacode.org/wiki/Modular_inverse#JavaScript
+function modInverse(a, b) {
+  a %= b;
+  for (let x = 1; x < b; x++) {
+    if ((a * x) % b === 1) {
+      return x;
     }
   }
-
-  return -1;
-}
-
-function encode(str, num) {
-  let encodeStr = '';
-  for (let alphabet of str) {
-    const position = getPositionOfCharacter(alphabet);
-    const value = (position * num) % 26;
-    encodeStr += getCharacterFromPosition(value);
-  }
-
-  return encodeStr;
+  return 1;
 }
 
 function decode(text) {
   const num = parseInt(text);
   const str = text.split(num)[1];
-  console.log(num, str);
   let originalStr = '';
+  const modInverseValue = modInverse(num, 26);
+  if (modInverseValue == 1) return 'Impossible to decode';
+  // 12 * 6015 % 26 = 4
+
   for (const alphabet of str) {
     const position = getPositionOfCharacter(alphabet);
-    const result = solveEquation(num, position);
-    if (result === -1) return 'Impossible to decode';
-    const encodedAlphabet = String.fromCharCode(97 + result);
-    originalStr += encodedAlphabet;
+    const value = (position * modInverseValue) % 26;
+    const orignalAlphabet = getCharacterFromPosition(value);
+    originalStr += orignalAlphabet;
   }
 
-  if (encode(originalStr, num) !== str) return 'Impossible to decode';
   return originalStr;
 }
 
-console.log(decode('5057aan'));
 module.exports = { decode };
