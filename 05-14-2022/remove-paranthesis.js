aba335eb7d9c9fb8a8400cf46a383129cfb6a25c
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-05-14 23:39:36 +0530

    :green_heart: solved remove parentheses stack based problem

---
+++ 05-14-2022/remove-paranthesis.js
@@ -0,0 +1,34 @@
+function removeParentheses(s) {
+  const stack = [];
+  for (const char of s) {
+    if (char === ')') {
+      while (stack[stack.length - 1] !== '(') {
+        stack.pop();
+      }
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.join('');
+}
+
+console.log(
+  removeParentheses('example(unwanted thing)example'),
+  'exampleexample'
+);
+console.log(
+  removeParentheses('example (unwanted thing) example'),
+  'example  example'
+);
+console.log(removeParentheses('a (bc d)e'), 'a e');
+console.log(removeParentheses('a(b(c))'), 'a');
+console.log(
+  removeParentheses('hello example (words(more words) here) something'),
+  'hello example  something'
+);
+console.log(
+  removeParentheses('(first group) (second group) (third group)'),
+  '  '
+);
