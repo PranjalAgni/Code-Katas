45f566f04e42c08c32164c0537c541bd631066c1
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-07-09 23:58:26 +0530

    :sparkles: solved vode kata

---
+++ 07-09-2022/compare-version.js
@@ -0,0 +1,28 @@
+function compareVersions(version1, version2) {
+  const version1List = version1.split('.');
+  const version2List = version2.split('.');
+  const N = version1List.length;
+  const M = version2List.length;
+  const min = Math.min(N, M);
+
+  for (let idx = 0; idx < min; idx++) {
+    const valA = parseInt(version1List[idx]);
+    const valB = parseInt(version2List[idx]);
+    if (valA === valB) continue;
+    else return valA > valB;
+  }
+
+  return N > M;
+}
+
+console.log(compareVersions('11', '10'));
+
+console.log(compareVersions('11', '11'));
+
+console.log(compareVersions('10.4.6', '10.4'));
+
+console.log(compareVersions('10.4', '11'));
+
+console.log(compareVersions('10.4', '10.10'));
+
+console.log(compareVersions('10.4.9', '10.5'));
