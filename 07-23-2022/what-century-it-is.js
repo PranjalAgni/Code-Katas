78a05836d5933c6008112a814b8573efaf484af2
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-07-23 23:36:31 +0530

    :rocket: solved 6kyu easy implementation kata

---
+++ 07-23-2022/what-century-it-is.js
@@ -0,0 +1,50 @@
+function addSuffix(year) {
+  if (year <= 20) return 'th';
+  const lastDigit = year % 10;
+  const suffixes = ['st', 'nd', 'rd', 'th'];
+  return suffixes[lastDigit - 1] ?? 'th';
+}
+
+function whatCentury(year) {
+  const lastTwoDigits = parseInt(year.substr(2, 2)) || 100;
+  const numYearsToMakeItCentury = 100 - lastTwoDigits;
+  const itsCenturyYear = parseInt(year) + numYearsToMakeItCentury;
+  const answer = itsCenturyYear.toString().substring(0, 2);
+  return answer + addSuffix(parseInt(answer));
+}
+
+console.log(
+  whatCentury('1999'),
+  '20th',
+  "With input '1999' solution produced wrong output"
+);
+console.log(
+  whatCentury('2011'),
+  '21st',
+  "With input '2011' solution produced wrong output"
+);
+console.log(
+  whatCentury('2154'),
+  '22nd',
+  "With input '2154' solution produced wrong output"
+);
+console.log(
+  whatCentury('2259'),
+  '23rd',
+  "With input '2259' solution produced wrong output"
+);
+console.log(
+  whatCentury('1234'),
+  '13th',
+  "With input '1234' solution produced wrong output"
+);
+console.log(
+  whatCentury('1023'),
+  '11th',
+  "With input '1023' solution produced wrong output"
+);
+console.log(
+  whatCentury('2000'),
+  '20th',
+  "With input '2000' solution produced wrong output"
+);
