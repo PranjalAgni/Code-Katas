d763da1f56ae30b265a3cf00738ba3d6bae2b8b9
PranjalAgni <pranjal.agnihotri6@gmail.com>
2022-09-27 00:05:34 +0530

    :sparkles: solved find missing letters

--- 09-26-2022/find-the-missing-letter.js
+++ 09-26-2022/find-the-missing-letter.js
@@ -1,5 +1,16 @@
 function findMissingLetter(array) {
-  return ' ';
+  let prevCharacter = array[0];
+  let N = array.length;
+  for (let idx = 1; idx < N; idx++) {
+    let currentCharacterShouldBe = String.fromCharCode(
+      prevCharacter.charCodeAt(0) + 1
+    );
+    if (currentCharacterShouldBe !== array[idx])
+      return currentCharacterShouldBe;
+    prevCharacter = array[idx];
+  }
+
+  return String.fromCharCode(array[N - 1].charCodeAt(0) + 1);
 }
 
 console.log(findMissingLetter(['a', 'b', 'c', 'd', 'f']), 'e');
