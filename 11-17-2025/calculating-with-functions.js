08f90743697c086568ee720b6023b7935694ccff
Pranjal Agnihotri <pranjal.agnihotri6@gmail.com>
2025-11-18 02:00:48 +0530

    :green_heart: solved calculating with functions based on fn chaining

--- 11-17-2025/calculating-with-functions.js
+++ 11-17-2025/calculating-with-functions.js
@@ -1,20 +1,74 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+function operator(args, value) {
+  const hasArgs = args.length > 0;
+  const leftOperand = value;
+  if (hasArgs) {
+    const [operator, rightOperand] = args[0];
+    return calculate(leftOperand, operator, rightOperand);
+  }
+  return leftOperand;
+}
 
-function plus() {}
-function minus() {}
-function times() {}
-function dividedBy() {}
+function calculate(leftOperand, operator, rightOperand) {
+  const operatorCalculationMapping = {
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '*': (a, b) => a * b,
+    '/': (a, b) => Math.floor(a / b),
+  };
 
-console.log(seven(times(five())), 35, 'seven(times(five ()))');
-console.log(four(plus(nine())), 13, 'four (plus(nine ()))');
-console.log(eight(minus(three())), 5, 'eight(minus(three()))');
-console.log(six(dividedBy(two())), 3, 'six(dividedBy(two()))');
+  return operatorCalculationMapping[operator](leftOperand, rightOperand);
+}
+
+function zero() {
+  return operator(arguments, 0);
+}
+
+function one() {
+  return operator(arguments, 1);
+}
+
+function two() {
+  return operator(arguments, 2);
+}
+function three() {
+  return operator(arguments, 3);
+}
+function four() {
+  return operator(arguments, 4);
+}
+function five() {
+  return operator(arguments, 5);
+}
+function six() {
+  return operator(arguments, 6);
+}
+function seven() {
+  return operator(arguments, 7);
+}
+function eight() {
+  return operator(arguments, 8);
+}
+function nine() {
+  return operator(arguments, 9);
+}
+
+function plus(args) {
+  console.log({ args, arguments__o: arguments });
+  const rightOperand = args;
+  return ['+', rightOperand];
+}
+
+function minus(args) {
+  const rightOperand = args;
+  return ['-', rightOperand];
+}
+
+function times(args) {
+  const rightOperand = args;
+  return ['*', rightOperand];
+}
+
+function dividedBy(args) {
+  const rightOperand = args;
+  return ['/', rightOperand];
+}
